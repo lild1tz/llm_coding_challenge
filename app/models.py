1036e29d90b6a7aa73ae3af068d4835b73cc1937
Александр <lild1tz2023@gmail.com>
2025-04-12 08:18:22 +0300

    small fix in models.py

--- app/models.py
+++ app/models.py
@@ -6,10 +6,10 @@ class TableRow(BaseModel):
     division: str = Field(..., description="Подразделение, выполнявшее работу (например, АОР, Юг, Мир и т.д.)")
     operation: str = Field(..., description="Наименование выполненной полевой операции (например, Пахота, Дискование и т.д.)")
     culture: str = Field(..., description="Сельскохозяйственная культура, к которой относится операция")
-    per_day: Union[int, float] = Field(..., description="Объём выполненных работ за день (в гектарах)")
-    per_operation: Union[int, float] = Field(..., description="Накопленный объём работ с начала операции (в гектарах)")
-    val_day: Union[int, float] = Field(None, description="Валовый сбор за день (в центнерах), если применимо")
-    val_beginning: Union[int, float] = Field(None, description="Суммарный валовый сбор с начала операции (в центнерах), если применимо")
+    per_day: Union[int, float, None] = Field(None, description="Объём выполненных работ за день (в гектарах)")
+    per_operation: Union[int, float, None] = Field(None, description="Накопленный объём работ с начала операции (в гектарах)")
+    val_day: Union[int, float, None] = Field(None, description="Валовый сбор за день (в центнерах), если применимо")
+    val_beginning: Union[int, float, None] = Field(None, description="Суммарный валовый сбор с начала операции (в центнерах), если применимо")
 
 class Output(BaseModel):
     table: List[TableRow]
