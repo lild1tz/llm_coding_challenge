a596faa9de5b036355ceee85797e592964ceb835
valerijhegaj <valerijhegaj@gmail.com>
2025-04-17 18:38:19 +0300

    more refactor

--- backend/hermes/internal/handlers/telegram/handler.go
+++ backend/hermes/internal/handlers/telegram/handler.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -15,92 +14,106 @@ import (
 )
 
 func NewHandler(
-	ctx context.Context,
+	shutdownCtx context.Context,
 	clients *clients.Clients,
 	repositories *repositories.Repositories,
 	recognizerManager *recognizer.Manager,
 ) func(ctx context.Context, update tgbotapi.Update) error {
-	return func(ctx context.Context, update tgbotapi.Update) error {
-		if update.Message == nil {
-			return nil
-		}
+	return (&Handler{clients, repositories, recognizerManager, shutdownCtx}).Handle
+}
 
-		telegramID := models.GetTelegramID(update)
-		chatName := models.GetTelegramChatName(update)
-		content := models.GetTelegramContent(update)
-		timestamp := models.GetTelegramTimestamp(update)
-		name := models.GetTelegramName(update)
+type Handler struct {
+	clients           *clients.Clients
+	repositories      *repositories.Repositories
+	recognizerManager *recognizer.Manager
+	shutdownCtx       context.Context
+}
 
-		fmt.Println("chatName", chatName)
-		fmt.Println("telegramID", telegramID)
-		fmt.Println("name", name)
-		fmt.Println("content", content)
-		fmt.Println("timestamp", timestamp)
-
-		found, err := repositories.ChatsRepo.FindChat(ctx, chatName)
-		if err != nil {
-			log.Printf("failed to find chat: %v", err)
-			return nil
-		}
-
-		if !found {
-			log.Printf("chat refused: %s", chatName)
-			return nil
-		}
-
-		if update.Message.Photo != nil || update.Message.Document != nil {
-			var fileID string
-
-			if update.Message.Photo != nil {
-				photoSize := update.Message.Photo[len(update.Message.Photo)-1]
-				fileID = photoSize.FileID
-			} else if update.Message.Document != nil {
-				fileID = update.Message.Document.FileID
-			}
-
-			fileLink, err := clients.Telegram.Bot.GetFileDirectURL(fileID)
-			if err != nil {
-				log.Printf("Ошибка получения ссылки на файл: %v", err)
-				return nil
-			}
-
-			resp, err := http.Get(fileLink)
-			if err != nil {
-				log.Printf("Ошибка загрузки файла: %v", err)
-				return nil
-			}
-			defer resp.Body.Close()
-
-			data, err := io.ReadAll(resp.Body)
-			if err != nil {
-				log.Printf("Ошибка чтения данных: %v", err)
-				return nil
-			}
-
-			go recognizerManager.AsyncProcessImageMessage(models.ImageMessage{
-				TextMessage: models.TextMessage{
-					TelegramID: &telegramID,
-					ChatName:   chatName,
-					Text:       content,
-					Timestamp:  timestamp,
-					Name:       name,
-				},
-				Image: data,
-			})
-		}
-
-		if update.Message.Text != "" {
-			go recognizerManager.AsyncProcessTextMessage(
-				models.TextMessage{
-					TelegramID: &telegramID,
-					ChatName:   chatName,
-					Text:       content,
-					Timestamp:  timestamp,
-					Name:       name,
-				},
-			)
-		}
-
+func (h *Handler) Handle(ctx context.Context, update tgbotapi.Update) error {
+	if update.Message == nil {
 		return nil
 	}
+
+	telegramID := models.GetTelegramID(update)
+
+	textMessage := models.TextMessage{
+		TelegramID: &telegramID,
+		ChatName:   models.GetTelegramChatName(update),
+		Text:       models.GetTelegramContent(update),
+		Timestamp:  models.GetTelegramTimestamp(update),
+		Name:       models.GetTelegramName(update),
+	}
+
+	fmt.Println("chatName", textMessage.ChatName)
+	fmt.Println("telegramID", textMessage.TelegramID)
+	fmt.Println("name", textMessage.Name)
+	fmt.Println("content", textMessage.Text)
+	fmt.Println("timestamp", textMessage.Timestamp)
+
+	found, err := h.repositories.ChatsRepo.FindChat(ctx, textMessage.ChatName)
+	if err != nil {
+		return fmt.Errorf("failed to find chat: %v", err)
+	}
+
+	if !found {
+		return fmt.Errorf("chat refused: %s", textMessage.ChatName)
+	}
+
+	if update.Message.Photo != nil || update.Message.Document != nil {
+		var fileID string
+
+		if update.Message.Photo != nil {
+			photoSize := update.Message.Photo[len(update.Message.Photo)-1]
+			fileID = photoSize.FileID
+		} else if update.Message.Document != nil {
+			fileID = update.Message.Document.FileID
+		}
+
+		fmt.Println("Тип: изображение")
+		fmt.Println("fileID", fileID)
+
+		go func() {
+			err := h.handleImageMessage(
+				h.shutdownCtx, fileID,
+				textMessage,
+			)
+			if err != nil {
+				fmt.Println("error in handleImageMessage", err)
+			}
+		}()
+	}
+
+	if update.Message.Text != "" {
+		fmt.Println("Тип: текст")
+		fmt.Println("Текст:", textMessage.Text)
+
+		go h.recognizerManager.AsyncProcessTextMessage(textMessage)
+	}
+
+	return nil
+}
+
+func (h *Handler) handleImageMessage(ctx context.Context, fileID string, textMessage models.TextMessage) error {
+	fileLink, err := h.clients.Telegram.Bot.GetFileDirectURL(fileID)
+	if err != nil {
+		return fmt.Errorf("Ошибка получения ссылки на файл: %v", err)
+	}
+
+	resp, err := http.Get(fileLink)
+	if err != nil {
+		return fmt.Errorf("Ошибка загрузки файла: %v", err)
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("Ошибка чтения данных: %v", err)
+	}
+
+	h.recognizerManager.AsyncProcessImageMessage(models.ImageMessage{
+		TextMessage: textMessage,
+		Image:       data,
+	})
+
+	return nil
 }
--- backend/hermes/internal/handlers/whatsapp/handler.go
+++ backend/hermes/internal/handlers/whatsapp/handler.go
@@ -12,100 +12,105 @@ import (
 	"github.com/lild1tz/llm_coding_challenge/backend/hermes/internal/managers/recognizer"
 	"github.com/lild1tz/llm_coding_challenge/backend/hermes/internal/models"
 	"github.com/lild1tz/llm_coding_challenge/backend/hermes/internal/repositories"
+	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/types/events"
 )
 
 func NewHandler(
-	ctx context.Context,
+	shutdownCtx context.Context,
 	clients *clients.Clients,
 	repositories *repositories.Repositories,
 	recognizerManager *recognizer.Manager,
 ) func(evt interface{}) {
-	return func(evt interface{}) {
-		if ctx.Err() != nil {
+	return (&Handler{clients, repositories, recognizerManager, shutdownCtx}).Handle
+}
+
+type Handler struct {
+	clients           *clients.Clients
+	repositories      *repositories.Repositories
+	recognizerManager *recognizer.Manager
+	shutdownCtx       context.Context
+}
+
+func (h *Handler) Handle(evt interface{}) {
+	if h.shutdownCtx.Err() != nil {
+		return
+	}
+
+	switch v := evt.(type) {
+	case *events.Message:
+		msg := v.Message
+
+		whatsappID := v.Info.Sender.String()
+		textMessage := models.TextMessage{
+			WhatsappID: &whatsappID,
+			ChatName:   v.Info.Chat.String(),
+			Name:       v.Info.PushName,
+			Timestamp:  v.Info.Timestamp,
+		}
+
+		fmt.Println("chatID", textMessage.ChatName)
+		fmt.Println("whatsappID", whatsappID)
+
+		found, err := h.repositories.ChatsRepo.FindChat(h.shutdownCtx, textMessage.ChatName)
+		if err != nil {
+			log.Printf("failed to find chat: %v", err)
 			return
 		}
 
-		switch v := evt.(type) {
-		case *events.Message:
-			msg := v.Message
-			whatsappID := v.Info.Sender.String()
-			chatName := v.Info.Chat.String()
-			name := v.Info.PushName
-			sendedAt := v.Info.Timestamp
+		if !found {
+			log.Printf("chat refused: %s", textMessage.ChatName)
+			return
+		}
 
-			fmt.Println("chatID", chatName)
-			fmt.Println("whatsappID", whatsappID)
+		if msg.Conversation != nil {
+			textMessage.Text = msg.GetConversation()
 
-			found, err := repositories.ChatsRepo.FindChat(ctx, chatName)
+			fmt.Println("Тип: текст")
+			fmt.Println("Текст:", textMessage.Text)
+
+			go h.recognizerManager.AsyncProcessTextMessage(textMessage)
+		} else if msg.ImageMessage != nil {
+			fmt.Println("Тип: изображение")
+			fmt.Println("URL:", msg.ImageMessage.GetURL())
+
+			go h.handleImageMessage(h.shutdownCtx, msg.GetImageMessage(), textMessage)
+		} else if msg.AudioMessage != nil {
+			// Generate a unique filename (e.g., timestamp)
+			filename := fmt.Sprintf("audio_%d.oga", time.Now().Unix())
+
+			// Download the audio data
+			audioData, err := h.clients.Whatsapp.Download(msg.AudioMessage)
 			if err != nil {
-				log.Printf("failed to find chat: %v", err)
+				fmt.Printf("Failed to download audio: %v\n", err)
 				return
 			}
 
-			if !found {
-				log.Printf("chat refused: %s", chatName)
+			err = os.WriteFile(filename, audioData, 0644)
+			if err != nil {
+				fmt.Printf("Failed to save audio: %v\n", err)
 				return
 			}
 
-			if msg.Conversation != nil {
-				content := msg.GetConversation()
-				fmt.Println("Тип: простой текст")
-				fmt.Println("Текст:", content)
-
-				go recognizerManager.AsyncProcessTextMessage(
-					models.TextMessage{
-						WhatsappID: &whatsappID,
-						TelegramID: nil,
-						ChatName:   chatName,
-						Name:       name,
-						Timestamp:  sendedAt,
-						Text:       content,
-					},
-				)
-			} else if msg.ImageMessage != nil {
-				fmt.Println("Тип: изображение")
-				fmt.Println("URL:", msg.ImageMessage.GetURL())
-
-				data, err := clients.Whatsapp.Download(msg.GetImageMessage())
-				if err != nil {
-					log.Printf("failed to download image: %v", err)
-					return
-				}
-
-				mime := mimetype.Detect(data)
-				fmt.Printf("Detected MIME type: %s\n", mime.String())
-
-				go recognizerManager.AsyncProcessImageMessage(models.ImageMessage{
-					TextMessage: models.TextMessage{
-						WhatsappID: &whatsappID,
-						TelegramID: nil,
-						ChatName:   chatName,
-						Name:       name,
-						Timestamp:  sendedAt,
-					},
-					Image: data,
-				})
-			} else if msg.AudioMessage != nil {
-				// Generate a unique filename (e.g., timestamp)
-				filename := fmt.Sprintf("audio_%d.oga", time.Now().Unix())
-
-				// Download the audio data
-				audioData, err := clients.Whatsapp.Download(msg.AudioMessage)
-				if err != nil {
-					fmt.Printf("Failed to download audio: %v\n", err)
-					return
-				}
-
-				// Save to a file
-				err = os.WriteFile(filename, audioData, 0644)
-				if err != nil {
-					fmt.Printf("Failed to save audio: %v\n", err)
-					return
-				}
-
-				fmt.Printf("Saved audio to %s\n", filename)
-			}
+			fmt.Printf("Saved audio to %s\n", filename)
 		}
 	}
+}
+
+func (h *Handler) handleImageMessage(ctx context.Context, msg whatsmeow.DownloadableMessage, textMessage models.TextMessage) error {
+	data, err := h.clients.Whatsapp.Download(msg)
+	if err != nil {
+		log.Printf("failed to download image: %v", err)
+		return err
+	}
+
+	mime := mimetype.Detect(data)
+	fmt.Printf("Detected MIME type: %s\n", mime.String())
+
+	h.recognizerManager.AsyncProcessImageMessage(models.ImageMessage{
+		TextMessage: textMessage,
+		Image:       data,
+	})
+
+	return nil
 }
